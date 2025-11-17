724dff106c63d7b943742a3df79712e1ae4fe792
Karen Torres <kjtorres.creativo@gmail.com>
2025-11-17 12:59:36 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ controls.update();
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-cube.position.set(0, 5, -5);
+cube.position.set(0, 2, 0);
 scene.add( cube );
 
 camera.position.z = 5;
